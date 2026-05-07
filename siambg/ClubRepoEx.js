fc4caefd96229552062751064ac2b37b4550f418
Modiholodri <reinhold.lauer@gmail.com>
2026-05-07 20:07:16 +0700

    Add Club Repo Example.js without PAT to the repo  #501

---
+++ siambg/ClubRepoEx.js
@@ -0,0 +1,27 @@
+const repoOwner = 'modiholodri';  // Repo Owner for the Club Repo
+
+const clubRepos = [   // Club Repos list, put the default one first
+    {
+        name: 'Siam Backgammon',
+        tpye: 'Merger',
+        repo: 'siambg-ranking-list',
+        suffix: 'siam',
+    },
+    {
+        name: 'Bangkok Backgammon',
+        tpye: 'Club',
+        repo: 'bkk-bg-rating-list',
+        swuffix: 'bkk',
+    },
+    {
+        name: 'Chiang Mai Backgammon',
+        tpye: 'Club',
+        repo: 'cnxbg-ranking-list',
+        suffix: 'cnx',
+    },
+];
+
+const githubToken = '';  // Fill in the PAT before publishing
+
+const tournamentDay = 6; // Day of the week when the tournament is held
+const initialRating = 1800; // Initial rating for new players, does not effect the rating calculation on GitHub
