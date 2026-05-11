e28f064caf05dd186e00ac355942ad3d4d41c653
Modiholodri <reinhold.lauer@gmail.com>
2026-05-11 17:23:32 +0700

    Expand the Match Report and Tournament section by default #513

--- fakbg/Settings.js
+++ fakbg/Settings.js
@@ -5,8 +5,8 @@ document.addEventListener('DOMContentLoaded', loadSettings);
 window.addEventListener('beforeunload', saveSettings);
 
 // load the toggle settings of a specific toggle element
-function loadToggleSetting(elementName) {
-    const toggleState = localStorage.getItem(elementName + 'State') || 'collapsed';
+function loadToggleSetting(elementName, defaultState = 'collapsed') {
+    const toggleState = localStorage.getItem(elementName + 'State') || defaultState;
     const element = document.getElementById(elementName);
     toggleState === 'expanded' ? element.classList.add('show') : element.classList.remove('show');
 }
@@ -22,9 +22,9 @@ function loadValueSetting(elementName, defaultValue) {
 
 // Load settings from localStorage
 function loadSettings() {
-    loadToggleSetting('matchReportForm');
+    loadToggleSetting('matchReportForm', 'expanded');
 
-    loadToggleSetting('tournamentManagement');
+    loadToggleSetting('tournamentManagement', 'expanded');
     loadToggleSetting('tournamentDirector');
 
     // Tournament Management
